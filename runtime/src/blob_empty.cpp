a60ffe918e4f149690aa74c16d92f53f1facfc57
Lothilic <63964537+lothifia@users.noreply.github.com>
2026-04-28 18:23:48 +0800

    Initial embedded libtorch FactorMLP runtime

---
+++ runtime/src/blob_empty.cpp
@@ -0,0 +1,25 @@
+#include "blob.h"
+
+const uint8_t* embedded_encrypted_weights_data() {
+    return nullptr;
+}
+
+size_t embedded_encrypted_weights_size() {
+    return 0;
+}
+
+const uint8_t* embedded_weights_key_data() {
+    return nullptr;
+}
+
+size_t embedded_weights_key_size() {
+    return 0;
+}
+
+const uint8_t* embedded_weights_nonce_data() {
+    return nullptr;
+}
+
+size_t embedded_weights_nonce_size() {
+    return 0;
+}
