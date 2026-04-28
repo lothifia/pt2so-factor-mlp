522aae254520e1ce989361b970235f5d7413ecbf
Lothilic <63964537+lothifia@users.noreply.github.com>
2026-04-28 18:23:01 +0800

    Initial embedded libtorch FactorMLP runtime

---
+++ runtime/include/model_api.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+#define MODEL_API_EXPORT __attribute__((visibility("default")))
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void* ModelHandle;
+
+enum ModelDType {
+    MODEL_FLOAT32 = 1,
+    MODEL_FLOAT16 = 2,
+    MODEL_FLOAT64 = 3,
+    MODEL_INT64 = 4,
+    MODEL_INT32 = 5,
+    MODEL_UINT8 = 6,
+    MODEL_BOOL = 7
+};
+
+struct TensorDesc {
+    void* data;
+    int dtype;
+    int ndim;
+    int64_t shape[8];
+    size_t bytes;
+};
+
+MODEL_API_EXPORT int model_create(ModelHandle* out_handle);
+
+MODEL_API_EXPORT int model_forward(
+    ModelHandle handle,
+    const TensorDesc* inputs,
+    int input_count,
+    TensorDesc* outputs,
+    int output_count);
+
+MODEL_API_EXPORT void model_destroy(ModelHandle handle);
+
+MODEL_API_EXPORT const char* model_last_error(ModelHandle handle);
+
+MODEL_API_EXPORT const char* model_version();
+
+#ifdef __cplusplus
+}
+#endif
