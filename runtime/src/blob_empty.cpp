66ba2bcbc795e1665569f39a51168bc5c55f0748
Lothilic <63964537+lothifia@users.noreply.github.com>
2026-04-29 10:43:39 +0800

    Load encryption key outside shared library

--- runtime/src/blob_empty.cpp
+++ runtime/src/blob_empty.cpp
@@ -7,11 +7,3 @@ const uint8_t* embedded_encrypted_weights_pack_data() {
 size_t embedded_encrypted_weights_pack_size() {
     return 0;
 }
-
-const uint8_t* embedded_weights_key_data() {
-    return nullptr;
-}
-
-size_t embedded_weights_key_size() {
-    return 0;
-}
