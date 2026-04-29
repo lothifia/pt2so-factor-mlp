f6987946005fee868e42505fdf382a52d37790c7
Lothilic <63964537+lothifia@users.noreply.github.com>
2026-04-29 10:43:24 +0800

    Load encryption key outside shared library

--- runtime/include/blob.h
+++ runtime/include/blob.h
@@ -5,6 +5,3 @@
 
 const uint8_t* embedded_encrypted_weights_pack_data();
 size_t embedded_encrypted_weights_pack_size();
-
-const uint8_t* embedded_weights_key_data();
-size_t embedded_weights_key_size();
