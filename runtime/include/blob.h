38c46a4be46c048a1f6d5ae0ac310158dc95cc0a
Lothilic <63964537+lothifia@users.noreply.github.com>
2026-04-29 10:30:26 +0800

    Use incbin for embedded encrypted weights

--- runtime/include/blob.h
+++ runtime/include/blob.h
@@ -3,11 +3,8 @@
 #include <cstddef>
 #include <cstdint>
 
-const uint8_t* embedded_encrypted_weights_data();
-size_t embedded_encrypted_weights_size();
+const uint8_t* embedded_encrypted_weights_pack_data();
+size_t embedded_encrypted_weights_pack_size();
 
 const uint8_t* embedded_weights_key_data();
 size_t embedded_weights_key_size();
-
-const uint8_t* embedded_weights_nonce_data();
-size_t embedded_weights_nonce_size();
